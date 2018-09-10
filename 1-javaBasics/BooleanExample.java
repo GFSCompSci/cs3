704c96f704296e66a122eed0db9844a0d87544c6
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2018-09-10 15:38:58 -0400

    java examples

---
+++ 1-javaBasics/BooleanExample.java
@@ -0,0 +1,11 @@
+
+// A Java program to demonstrate boolean data type
+class BooleanExample 
+{
+    public static void main(String args[])
+    {
+        boolean b = true;
+        if (b == true)
+            System.out.println("Hi Geek");
+    }
+}
