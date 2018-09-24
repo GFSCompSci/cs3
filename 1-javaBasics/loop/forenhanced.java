beac555a05eaaa2160acfd291d5debcb53e5790b
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2018-09-23 22:50:44 -0400

    loop/if

---
+++ 1-javaBasics/loop/forenhanced.java
@@ -0,0 +1,8 @@
+class ForLoopExample {
+   public static void main(String args[]){
+      int arr[]={2,11,45,9};
+      for (int num : arr) {
+         System.out.println(num);
+      }
+   }
+}
---
+++ 1-javaBasics/loop/naturalfor.java
@@ -0,0 +1,13 @@
+public class Demo {
+
+    public static void main(String[] args) {
+
+       int num = 10, count, total = 0;
+
+       for(count = 1; count <= num; count++){
+           total = total + count;
+       }
+
+       System.out.println("Sum of first 10 natural numbers is: "+total);
+    }
+}
